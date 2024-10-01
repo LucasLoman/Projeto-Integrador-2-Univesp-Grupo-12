0af0dc193e5cff10896974fdf4077cd0cf31e039
GabrielFerreiraS-GFS <159969758+GabrielFerreiraS-GFS@users.noreply.github.com>
2024-10-01 17:54:42 -0300

    Branch Gabriel
    Publicando minha branch depois de arrumar o problema com o ambiente virtual, começarei a testar a criação de um banco de dados agora

--- projeto/Scripts/activate.bat
+++ projeto/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\lucas\OneDrive\Área de Trabalho\PI_2\projeto
+set VIRTUAL_ENV=C:\Users\Gabriel\Documents\GitHub\Projeto-Integrador-2-Univesp-Grupo-12\projeto
 
 if not defined PROMPT set PROMPT=$P$G
 
